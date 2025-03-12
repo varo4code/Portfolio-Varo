974376cb9945a121ca149eb4dd39bfc9fe0e7c02
VaroCode <varo4code@gmail.com>
2025-03-12 09:47:08 +0100

    Checkpoint updated data

--- src/app/layout/projects/projects.component.ts
+++ src/app/layout/projects/projects.component.ts
@@ -7,76 +7,91 @@ import { TranslateModule } from '@ngx-translate/core';
   standalone: true,
   imports: [ProjectCardComponent, TranslateModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.sass'
+  styleUrl: './projects.component.sass',
 })
 export class ProjectsComponent {
   selected: string = 'websites';
 
   websites = [
-    // {
-    //   title: 'Santander Digital Services',
-    //   comment: "Development and enhancement of various internal tools for Banco Santander's management processes over a period of more than three years.",
-    //   image: 'assets/projects/santander.webp',
-    //   technologies: ['Java', 'Maven', 'Spring Boot','PHP','Laravel','CSS3','Html5']
-    // },
+    {
+      title: 'Portfolio',
+      comment: 'You are here.',
+      image: 'assets/projects/portfolio.webp',
+      technologies: ['NodeJS', 'Angular', 'TypeScript', 'SASS', 'Html5'],
+    },
+    {
+      title: 'Santander Digital Services',
+      comment:
+        "Development and enhancement of various internal tools for Banco Santander's management processes over a period of more than three years.",
+      image: 'assets/projects/santander.webp',
+      technologies: [
+        'Java',
+        'Maven',
+        'Spring Boot',
+        'PHP',
+        'Laravel',
+        'Angular',
+        'CSS3',
+        'Html5',
+      ],
+    },
     {
       title: 'Telefónica',
       // comment: "Migration of Telefónica's website to WordPress VIP, with the development of a custom theme and a plugin with exclusive blocks for the Gutenberg editor.",
       image: 'assets/projects/telefonica.webp',
-      technologies: ['PHP','Wordpress','JavaScript','CSS3','Html5'],
+      technologies: ['PHP', 'Wordpress', 'JavaScript', 'CSS3', 'Html5'],
       linkName: 'telefonica.es',
-      link: 'https://telefonica.es/'
+      link: 'https://telefonica.es/',
     },
     {
       title: 'RCI Connect',
       image: 'assets/projects/rciconnect.webp',
-      technologies: ['NodeJS','Angular','TypeScript','PHP','CSS3','Html5'],
+      technologies: ['NodeJS', 'Angular', 'TypeScript', 'PHP', 'CSS3', 'Html5'],
       linkName: 'rciconnect.es',
-      link: 'https://rciconnect.es/'
+      link: 'https://rciconnect.es/',
     },
     {
       title: 'Kaiku Caffe Latte',
       image: 'assets/projects/kaiku.webp',
-      technologies: ['PHP','Wordpress','JavaScript','CSS3','Html5'],
+      technologies: ['PHP', 'Wordpress', 'JavaScript', 'CSS3', 'Html5'],
       linkName: 'kaikucaffelatte.com',
-      link: 'https://kaikucaffelatte.com/'
+      link: 'https://kaikucaffelatte.com/',
     },
     {
       title: 'Kaiku Sin Lactosa',
       image: 'assets/projects/kaikusinlactosa.webp',
-      technologies: ['PHP','Wordpress','JavaScript','CSS3','Html5'],
+      technologies: ['PHP', 'Wordpress', 'JavaScript', 'CSS3', 'Html5'],
       linkName: 'kaikusinlactosa.com',
-      link: 'https://kaikusinlactosa.com/'
+      link: 'https://kaikusinlactosa.com/',
     },
     {
       title: 'Formación Hosteleria',
       image: 'assets/projects/formacionhosteleria.webp',
-      technologies: ['PHP','Wordpress','JavaScript','CSS3','Html5'],
+      technologies: ['PHP', 'Wordpress', 'JavaScript', 'CSS3', 'Html5'],
       linkName: 'formacionhosteleria.es',
-      link: 'https://formacionhosteleria.es/'
+      link: 'https://formacionhosteleria.es/',
     },
     {
       title: 'Isis Informatica',
       image: 'assets/projects/isis.webp',
-      technologies: ['PHP','Wordpress','JavaScript','CSS3','Html5'],
+      technologies: ['PHP', 'Wordpress', 'JavaScript', 'CSS3', 'Html5'],
       linkName: 'isisinformatica.es',
-      link: 'https://isisinformatica.es/'
-    }
+      link: 'https://isisinformatica.es/',
+    },
   ];
 
   projects = [
     {
       title: 'Portfolio',
-      comment: "You are here.",
+      comment: 'You are here.',
       image: 'assets/projects/portfolio.webp',
-      technologies: ['NodeJS', 'Angular', 'TypeScript', 'SASS', 'Html5']
-    }
+      technologies: ['NodeJS', 'Angular', 'TypeScript', 'SASS', 'Html5'],
+    },
   ];
 
-  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   select(option: string) {
     this.selected = option;
   }
-
 }
